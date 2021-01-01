ef137ccc279c16f42ef198765b80fbf7e03e176f
Marlowe <marlowe1999@hotmail.co.uk>
2021-01-01 02:20:46 +0000

    fixed chair fart to folder

--- Markdowntohtml2999.js
+++
@@ -1,6 +0,0 @@
-window.onload = function () {
-	let toConvert = document.getElementsByClassName('markdown');
-	for (let i = 0; i < toConvert.length; i++) {
-		toConvert[i].innerHTML = marked(toConvert[i].innerHTML);
-	}
-}
